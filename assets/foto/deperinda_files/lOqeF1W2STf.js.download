dca873c3254bd8ffe2d974ec02b39fa4453235ac
AgungAngely <agungstivencahyatif52120014@gmail.com>
2023-07-17 09:25:27 +0800

    commit pertama

---
+++ assets/foto/deperinda_files/lOqeF1W2STf.js.download
@@ -0,0 +1,4 @@
+;/*FB_PKG_DELIM*/
+
+__d("LSClearAllPublicChatEventStartAndEndTimes",[],(function(a,b,c,d,e,f){function a(){var a=arguments,b=a[a.length-1];b.n;var c=[];return b.resolve(c)}b=a;f["default"]=b}),66);
+__d("LSDeleteAllPublicChatFbEventData",["LSClearAllPublicChatEventStartAndEndTimes"],(function(a,b,c,d,e,f){function a(){var a=arguments,c=a[a.length-1];c.n;var d=[];return c.seq([function(a){return c.sp(b("LSClearAllPublicChatEventStartAndEndTimes"))},function(a){return c.resolve(d)}])}c=a;f["default"]=c}),66);
---
+++ view/navbar.php
@@ -0,0 +1,57 @@
+<div id="app">
+   <div class="main-wrapper">
+      <div class="navbar-bg"></div>
+      <nav class="navbar navbar-expand-lg main-navbar">
+         <form class="form-inline mr-auto">
+            <ul class="navbar-nav mr-3">
+               <li><a href="#" data-toggle="sidebar" class="nav-link nav-link-lg"><i class="fas fa-bars"></i></a></li>
+               <li><a href="#" data-toggle="search" class="nav-link nav-link-lg d-sm-none"><i class="fas fa-search"></i></a></li>
+            </ul>
+         </form>
+         <ul class="navbar-nav navbar-right">
+            <li class="dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
+                  <img alt="image" src="assets/template/assets/img/avatar/avatar-1.png" class="rounded-circle mr-1">
+                  <div class="d-sm-none d-lg-inline-block">Hi, Ujang Maman</div>
+               </a>
+               <div class="dropdown-menu dropdown-menu-right">
+                  <div class="dropdown-title">Logged in 5 min ago</div>
+                  <a href="features-profile.html" class="dropdown-item has-icon">
+                     <i class="far fa-user"></i> Profile
+                  </a>
+                  <a href="features-activities.html" class="dropdown-item has-icon">
+                     <i class="fas fa-bolt"></i> Activities
+                  </a>
+                  <a href="features-settings.html" class="dropdown-item has-icon">
+                     <i class="fas fa-cog"></i> Settings
+                  </a>
+                  <div class="dropdown-divider"></div>
+                  <a href="#" class="dropdown-item has-icon text-danger">
+                     <i class="fas fa-sign-out-alt"></i> Logout
+                  </a>
+               </div>
+            </li>
+         </ul>
+      </nav>
+      <div class="main-sidebar">
+         <aside id="sidebar-wrapper">
+            <div class="sidebar-brand">
+               <a href="index.php">Weighted Product</a>
+            </div>
+            <div class="sidebar-brand sidebar-brand-sm">
+               <a href="index.html">St</a>
+            </div>
+            <ul class="sidebar-menu">
+
+               <li class="menu-header">Dashboard</li>
+               <li class=""><a class="nav-link" href="index.php"><i class="fas fa-fire"></i> <span>Dashboard</span></a></li>
+               <li class="menu-header">alternative</li>
+               <li class=""><a class="nav-link" href="alternative.php"><i class="fas fa-th"></i> <span>input alternative</span></a></li>
+               <li class="menu-header">Kriteria</li>
+               <li class=""><a class="nav-link" href="kriteria.php"><i class="far fa-file-alt"></i> <span>Input bobot</span></a></li>
+               <li class="menu-header">Penilaian</li>
+               <li class=""><a class="nav-link" href="penilaian.php"><i class="fas fa-pencil-ruler"></i> <span>Input Penilaian</span></a></li>
+               <li class="menu-header">Weighted Product</li>
+               <li class=""><a class="nav-link" href="perangkingan.php"><i class="fas fa-rocket"></i> <span>Proses Perhitungan</span></a></li>
+            </ul>
+         </aside>
+      </div>
